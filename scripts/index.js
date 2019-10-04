15029349d2d56f11c16e0c84eabac6dc48e22400
Lily Keeling <lily.keeling@gmail.com>
2019-10-04 09:33:11 +0100

    bombs

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,42 @@
-
 class Game {
-  constructor(level, squares, grid) {
+  constructor(level, squares, grid, bombNumber, width) {
     // this.bombs = bombs,
     // this.player = player,
     this.level = level,
     this.squares = squares,
-    this.grid = grid
+    this.grid = grid,
+    this.bombNumber = bombNumber,
+    this.width = width,
+    
+    this.makeGrid = function() {
+      console.log(width)
+      for(let i = 0; i < this.width * this.width ; i ++ ) {
+        this.grid =   document.querySelector('.grid')
+        const square = document.createElement('div')
+        this.grid.append(square)
+        square.classList.add('grid-item')
+        square.dataset.index = i
+        this.squares.push(this.square)
+      }
 
+    }
 
   }
 
+  bombs() {
+    switch(this.level) {
+      case 1:
+        this.width = 9
+        this.bombNumber = 10
+        break
+      case 2:
+        this.width = 18
+        this.bombNumber = 30
+        break
+      case 3:
+        this.width = 36
+        this.bombNumber = 90
 
-
-  makeGrid(){
-    for(let i = 0; i < this.level * this.level ; i ++ ) {
-      this.grid =   document.querySelector('.grid')
-      const square = document.createElement('div')
-      console.log(this.level)
-      this.grid.append(square)
-      square.classList.add('grid-item')
-      square.dataset.index = i
-      this.squares.push(this.square)
     }
 
   }
@@ -29,11 +45,24 @@ class Game {
 
 }
 
-const mineSweeper = new Game(9, [])
 
 
-const init = () => {
+
+
+
+
+
+
+
+
+const mineSweeper = new Game(1, [])
+
+
+const init = () =>{
+  mineSweeper.bombs()
+
   mineSweeper.makeGrid()
+
 }
 
 
